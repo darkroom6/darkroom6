b2a0db469d4eb9ee0881b7c58493e2df592bb735
Anph <anph@flomail.net>
2023-08-10 22:15:45 +0700

    re-struture

--- js/message.js
+++ js/message.js
@@ -1,115 +1,145 @@
-const chatModule = (() => {
-  function appendMessage(msg, chatDelay = 100) {
-    if (!msg) return;
-    msg.name = msg.name || "ms" + +new Date();
-    const chatMessageList = document.querySelector('#chat_list .chat-message-list');
-    const msgname = "." + msg.name;
-    const msginner = ".messageinner-" + msg.name;
-    const chatListItem = document.createElement('li');
-    chatListItem.className = "message-" + msg.align + " " + msg.name;
-    const messageInnerDiv = document.createElement('div');
-    messageInnerDiv.className = "messageinner-" + msg.name;
-    const messageContent = "<span class='message-text'>" + msg.msg + "</span>";
-    messageInnerDiv.innerHTML = messageContent;
-    chatListItem.appendChild(messageInnerDiv);
-    chatMessageList.appendChild(chatListItem);
+const ChatModule = (function () {
+  let instance;
+  function init(chatMessagesDefault, chatMessages, notiContent) {
+    let chatModule = {
+      chatMessagesDefault,
+      chatMessages,
+      notiContent
+    };
 
-    setTimeout(function () {
-      document.querySelector(msgname).classList.add('fade-in');
-      onRowAdded();
-    }, chatDelay);
 
-    setTimeout(function () {
-      document.querySelector(msginner).classList.add('fade-in');
-      onRowAdded();
-    }, chatDelay + msg.delay + 10);
-  }
+    chatModule.appendMessage = function (msg, chatDelay = 100) {
+      if (!msg) return;
+      msg.name = msg.name || "ms" + +new Date();
+      const chatMessageList = document.querySelector('#chat_list .chat-message-list');
+      const msgname = "." + msg.name;
+      const msginner = ".messageinner-" + msg.name;
+      const chatListItem = document.createElement('li');
+      chatListItem.className = "message-" + msg.align + " " + msg.name;
+      const messageInnerDiv = document.createElement('div');
+      messageInnerDiv.className = "messageinner-" + msg.name;
+      const messageContent = "<span class='message-text'>" + msg.msg + "</span>";
+      messageInnerDiv.innerHTML = messageContent;
+      chatListItem.appendChild(messageInnerDiv);
+      chatMessageList.appendChild(chatListItem);
 
-  function handleKeyUp(event) {
-    if (event.keyCode === 13) {
-      sendMyChat();
-    } else if (event.ctrlKey) {
-      if (event.code === "KeyM") {
-        setTimeout(() => {
-          document.querySelector('#typing').classList.remove('hidden')
-        }, 100);
+      setTimeout(function () {
+        document.querySelector(msgname).classList.add('fade-in');
+        chatModule.onRowAdded();
+      }, chatDelay);
 
-        const msg = chatMessages.shift();
-        setTimeout(() => {
-          appendMessage(msg);
-          document.querySelector('#typing').classList.add('hidden');
-        }, msg.typing ? 2000 : 100);
+      setTimeout(function () {
+        document.querySelector(msginner).classList.add('fade-in');
+        chatModule.onRowAdded();
+      }, chatDelay + msg.delay + 10);
+    };
+
+    chatModule.handleKeyUp = function (event) {
+      if (event.keyCode === 13) {
+        chatModule.sendMyChat();
+      } else if (event.ctrlKey) {
+        if (event.code === "KeyM") {
+          document.querySelector('#typing').classList.remove('hidden');
+
+          const msg = chatModule.chatMessages.shift();
+          setTimeout(() => {
+            chatModule.appendMessage(msg);
+            document.querySelector('#typing').classList.add('hidden');
+          }, msg?.typing ? 2000 : 100);
+
+        } else if (event.code === "KeyN") {
+          chatModule.showNoti(chatModule.notiContent[1], 'nd');
+        }
       }
-    }
-  }
+    };
 
-  function sendMyChat() {
-    appendMessage({
-      msg: document.getElementById('chat_input').value,
-      delay: 100,
-      align: "right"
-    });
-    document.getElementById('chat_input').value = '';
-  }
+    chatModule.sendMyChat = function () {
+      chatModule.appendMessage({
+        msg: document.getElementById('chat_input').value,
+        delay: 100,
+        align: "right"
+      });
+      document.getElementById('chat_input').value = '';
+    };
 
-  function onRowAdded() {
-    const chatContainer = document.querySelector('#chat_list .chat-container');
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-  }
+    chatModule.onRowAdded = function () {
+      const chatContainer = document.querySelector('#chat_list .chat-container');
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    };
 
-  function showChatWindow() {
-    document.getElementById('notification_container').classList.remove('show');
-    document.getElementById('chat_window').classList.add('show');
-    document.getElementById('be_btn').classList.add('active')
-  }
+    chatModule.showChatWindow = function () {
+      document.getElementById('notification_container').classList.remove('show');
+      document.getElementById('chat_window').classList.add('show');
+      document.getElementById('be_btn').classList.add('active');
+    };
 
-  function hideChatWindow(onlyWindow) {
-    return () => {
-      if (!onlyWindow) {
-        const messageContent = "Có tin nhắn mới của Beboy";
-        showNoti(messageContent);
-        document.getElementById('notification_container').classList.add('show');
+    chatModule.hideChatWindow = function (onlyWindow) {
+      return function () {
+        if (!onlyWindow) {
+          chatModule.showNoti(chatModule.notiContent[0]);
+          document.getElementById('notification_container').classList.add('show');
+        }
+        document.getElementById('chat_window').classList.remove('show');
+        document.getElementById('be_btn').classList.remove('active');
+      };
+    };
+
+    chatModule.showNoti = function (notiContent, clsName) {
+      const noti = document.createElement('div');
+      noti.className = "CTinNhNMICABeboy text-center text-white text-2xl font-medium leading-[33.60px]";
+      noti.innerText = notiContent;
+      document.getElementById('notification_container').innerHTML = '';
+      document.getElementById('notification_container').appendChild(noti);
+      document.getElementById('notification_container').classList.add('show', clsName);
+      if (clsName) {
+        setTimeout(() => {
+          document.getElementById('notification_container').classList.remove('show');
+        }, 5e3);
       }
-      document.getElementById('chat_window').classList.remove('show');
-      document.getElementById('be_btn').classList.remove('active')
+    };
+
+    chatModule.loadDefaultMsg = function () {
+      chatModule.chatMessagesDefault?.forEach((msg, i) => {
+        chatModule.appendMessage(msg, 110 + (i * 110));
+      });
+    };
+
+    chatModule.onLoad = function () {
+      chatModule.loadDefaultMsg();
+      // Add event listener to the document for keyup events
+      document.getElementById('chat_input').addEventListener('keyup', function (event) {
+        chatModule.handleKeyUp(event);
+      });
+      // Add event listener to the document for keyup events
+      document.getElementById('send_chat').addEventListener('click', function () {
+        chatModule.sendMyChat();
+      });
+      //
+      document.getElementById('notification_container').addEventListener('click', function () {
+        chatModule.showChatWindow();
+      });
+      //
+      document.getElementById('be_btn').addEventListener('click', function () {
+        chatModule.showChatWindow();
+      });
+      //
+      document.getElementById('close_chat_window').addEventListener('click', function () {
+        chatModule.hideChatWindow(1)();
+      });
+      //
+      setTimeout(function () {
+        chatModule.hideChatWindow(0)();
+      }, 1000);
     }
+    return chatModule;
   }
 
-  function showNoti(messageContent) {
-    const noti = document.createElement('div');
-    noti.className = "CTinNhNMICABeboy text-center text-white text-2xl font-medium leading-[33.60px]";
-    noti.textContent = messageContent;
-    document.getElementById('notification_container').appendChild(noti);
-  }
-
-  function loadDefaultMsg(chatMessagesDefault) {
-    chatMessagesDefault?.forEach((msg, i) => {
-      appendMessage(msg, 110 + (i * 110));
-    });
-  }
-
-  function ChatModule(chatMessagesDefault, chatMessages, notiContent) {
-    this.chatMessagesDefault = chatMessagesDefault || [];
-    this.chatMessages = chatMessages || [];
-    this.notiContent = notiContent || "";
-  }
-
-  ChatModule.prototype.initialize = function () {
-    const self = this;
-
-    document.addEventListener('DOMContentLoaded', () => {
-      loadDefaultMsg(self.chatMessagesDefault);
-      document.getElementById('chat_input').addEventListener('keyup', handleKeyUp);
-      document.getElementById('send_chat').addEventListener('click', sendMyChat);
-      document.getElementById('notification_container').addEventListener('click', showChatWindow);
-      document.getElementById('be_btn').addEventListener('click', showChatWindow);
-      document.getElementById('close_chat_window').addEventListener('click', hideChatWindow(1));
-      showNoti(self.notiContent);
-      setTimeout(() => hideChatWindow(0), 1000);
-    });
+  return {
+    getInstance: function (chatMessagesDefault, chatMessages, notiContent) {
+      if (!instance) {
+        instance = init(chatMessagesDefault, chatMessages, notiContent);
+      }
+      return instance;
+    }
   };
-
-  return ChatModule;
 })();
-
-export default chatModule;
--- js/tap-7.js
+++ js/tap-7.js
@@ -1,3 +1,4 @@
+
 const chatMessagesDefault = [{
   name: 'ms1',
   msg: "K đang gặp H.",
@@ -8,125 +9,39 @@ const chatMessagesDefault = [{
   msg: "K & H sẽ đến nhà ông Khê.",
   delay: 100,
   align: "left"
-}];
+},
+];
 
 let chatMessages = [{
   msg: "Tiền của tôi đâu?",
   delay: 100,
   align: "left",
   typing: 1
+}, {
+  name: 'ms123',
+  msg: "Ổn rồi đúng không?",
+  delay: 100,
+  align: "left"
+},
+{
+  name: 'ms1234',
+  msg: "Hoàng đã nghe file của anh",
+  delay: 100,
+  align: "left"
+},
+{
+  name: 'ms12345',
+  msg: "Anh cũng nghe được Hoàng",
+  delay: 100,
+  align: "left"
+},
+{
+  name: 'ms12346',
+  msg: "Không ai khác nghe được những điều này",
+  delay: 100,
+  align: "left"
 }];
-
-/////////////////
-/////////////////
-/////////////////
-// Function to append messages to ChatWindows
-function appendMessage(msg, chatDelay = 100) {
-  if (!msg) return;
-  msg.name = msg.name || "ms" + +new Date();
-  const chatMessageList = document.querySelector('#chat_list .chat-message-list');
-  const msgname = "." + msg.name;
-  const msginner = ".messageinner-" + msg.name;
-  const chatListItem = document.createElement('li');
-  chatListItem.className = "message-" + msg.align + " " + msg.name;
-  const messageInnerDiv = document.createElement('div');
-  messageInnerDiv.className = "messageinner-" + msg.name;
-  const messageContent = "<span class='message-text'>" + msg.msg + "</span>";
-  messageInnerDiv.innerHTML = messageContent;
-  chatListItem.appendChild(messageInnerDiv);
-  chatMessageList.appendChild(chatListItem);
-
-  setTimeout(function () {
-    document.querySelector(msgname).classList.add('fade-in');
-    onRowAdded();
-  }, chatDelay);
-
-  setTimeout(function () {
-    document.querySelector(msginner).classList.add('fade-in');
-    onRowAdded();
-  }, chatDelay + msg.delay + 10);
-
-}
-
-// Function to handle keyup events globally
-function handleKeyUp(event) {
-  if (event.keyCode === 13) {
-    sendMyChat();
-  } else if (event.ctrlKey) {
-    if (event.code === "KeyM") {
-      setTimeout(() => {
-        document.querySelector('#typing').classList.remove('hidden')
-      }, 100);
-
-      const msg = chatMessages.shift();
-      setTimeout(() => {
-        appendMessage(msg);
-        document.querySelector('#typing').classList.add('hidden');
-      }, msg.typing ? 2000 : 100);
-    }
-  }
-}
-
-// Function to handle keyup events globally
-function sendMyChat() {
-  appendMessage({
-    msg: document.getElementById('chat_input').value,
-    delay: 100,
-    align: "right"
-  });
-  document.getElementById('chat_input').value = '';
-}
-
-
-function onRowAdded() {
-  const chatContainer = document.querySelector('#chat_list .chat-container');
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-}
-
-// Debounce function to introduce delay
-
-function showChatWindow() {
-  document.getElementById('notification_container').classList.remove('show');
-  document.getElementById('chat_window').classList.add('show');
-  document.getElementById('be_btn').classList.add('active')
-}
-
-function hideChatWindow(onlyWindow) {
-  return () => {
-    if (!onlyWindow) {
-      const messageContent = "Có tin nhắn mới của Beboy";
-      showNoti(messageContent);
-      document.getElementById('notification_container').classList.add('show');
-    }
-    document.getElementById('chat_window').classList.remove('show');
-    document.getElementById('be_btn').classList.remove('active')
-  }
-}
-function showNoti(messageContent) {
-  const noti = document.createElement('div');
-  noti.className = "CTinNhNMICABeboy text-center text-white text-2xl font-medium leading-[33.60px]";
-  noti.innerText = messageContent;
-  document.getElementById('notification_container').appendChild(noti);
-}
-
-function loadDefaultMsg() {
-  chatMessagesDefault?.forEach((msg, i) => {
-    appendMessage(msg, 110 + (i * 110));
-  });
-}
+const notiContent = ["Có tin nhắn mới của Beboy"];
+const chatModule = ChatModule.getInstance(chatMessagesDefault, chatMessages, notiContent);
 // Wait for the document to be fully loaded before adding the event listener
-document.addEventListener('DOMContentLoaded', () => {
-  loadDefaultMsg();
-  // Add event listener to the document for keyup events
-  document.getElementById('chat_input').addEventListener('keyup', handleKeyUp);
-  // Add event listener to the document for keyup events
-  document.getElementById('send_chat').addEventListener('click', sendMyChat);
-  //
-  document.getElementById('notification_container').addEventListener('click', showChatWindow);
-  //
-  document.getElementById('be_btn').addEventListener('click', showChatWindow);
-  //
-  document.getElementById('close_chat_window').addEventListener('click', hideChatWindow(1));
-  //
-  setTimeout(hideChatWindow(0), 1000);
-});
+document.addEventListener('DOMContentLoaded', chatModule.onLoad);
