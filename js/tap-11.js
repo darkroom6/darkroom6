41733636258155b42431c69b440dc4500a6529d0
Anph <anph@flomail.net>
2023-08-23 08:06:01 +0700

    Update tap-11.js

--- js/tap-11.js
+++ js/tap-11.js
@@ -7,27 +7,20 @@ const chatMessagesDefault = [
   }
 ];
 
-let chatMessages = [{
-  typing: true,
-  msg: 'Muốn tôi tắt máy?'
-}, {
-  msg: 'Nếu hết pin thì sao?',
-}, {
-  msg: "Đổi bằng cách nào?"
-}, {
-  typing: true,
-  msg: "Tôi đang sửa lại nội dung trị liệu cho Hoàng"
-}, {
-  msg: "Khi nào có thể cho Hoàng liều trị liệu mới", typing: 1
-}, {
-  msg: "Ta đã tính trước trường hợp này mà?",
-}, {
-  msg: "Đúng rồi!"
-}, {
-  msg: "Phương án Giao - Ninh",
-}, {
-  msg: "OK"
-}];
+let chatMessages = [
+  { typing: true, msg: 'Muốn tôi tắt máy?' },
+  { msg: 'Nếu hết pin thì sao?', },
+  { msg: 'Trong này không có chỗ cắm sạc.', },
+  { msg: "Đổi bằng cách nào?" },
+  { typing: true, msg: "Tôi đang sửa lại nội dung trị liệu cho Hoàng" },
+  { msg: "Khi nào có thể cho Hoàng liều trị liệu mới", typing: 1 },
+  { msg: "Ta đã tính trước trường hợp này mà?" },
+  { msg: "Đúng rồi!" },
+  { msg: "Phương án Giao - Ninh" },
+  { msg: "Liều đầu tiên đã hoạt động tốt", typing: 1 },
+  { msg: "Liều thứ 2 phải được dùng từ 5 đến 10 ngày sau liều đầu tiên", typing: 1 },
+  { msg: "OK" }
+];
 
 const notiContent = ["Có tin nhắn mới của GS Trọng"];
 
