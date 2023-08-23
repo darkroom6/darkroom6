ecc659f707e65387779e2910019116eca6a09693
Anph <anph@flomail.net>
2023-08-23 07:49:51 +0700

    Update tap-21.js

--- js/tap-21.js
+++ js/tap-21.js
@@ -4,7 +4,10 @@ let chatMessages = [
   { msg: "Ngoại giao. Tôi nhờ bạn của chúng ta trong này xạc giúp", typing: 1 },
   { msg: "Tôi cũng nghĩ vậy. Nhưng ta đâu có được quyết định.", typing: 1 },
   { msg: "Nó có biết nghe lén, xem lén được những cuộc nói này của ta không?", typing: 1 },
-  { msg: "Nhưng anh vẫn biết nó đi đâu làm gì?" }
+  { msg: "Nhưng anh vẫn biết nó đi đâu làm gì?" },
+  { msg: "Anh gom bằng cách nào?", typing: 1 },
+  { msg: "Tiến hành kế hoạch đi! Thời gian chín muồi rồi.", typing: 1 },
+  { msg: "Không trễ hơn được đâu! Vinh đã đánh hơi được rồi", typing: 1 }
 ];
 
 
