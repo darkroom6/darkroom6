1ec6b20b57cfd9029d2856496690ea524cdad7b6
darkroom6 <141474287+darkroom6@users.noreply.github.com>
2023-09-13 11:57:01 +0700

    Update tap-19.js

--- js/tap-19.js
+++ js/tap-19.js
@@ -1,33 +1,30 @@
 const chatMessagesDefault = [
-  {
-    name: 'ms1',
-    delay: 100, msg: 'Hey, tôi cần thêm 20 triệu', class2nd: 'be_chat'
-  },
-  {
-    name: 'ms2',
-    delay: 100, msg: 'Gấp!', class2nd: 'be_chat'
-  }
+ 
 ];
 
 let chatMessages = [
+   {
+    name: 'ms1',
+    delay: 100, msg: 'Ok',
+  },
+  {
+    name: 'ms2',
+    delay: 100, msg: 'Rồi',
+  }
 ];
 
 const notifications = [{
-  content: "Có tin nhắn mới của Beboy",
-  chatTo: "Beboy",
-  activeSender: '#be_chat',
+  content: "Có tin nhắn mới của Tuấn",
+  chatTo: "Tuấn",
+  activeSender: '#tuan_chat',
   msgCount: 1
-}, {
-  content: "GS Trọng is online",
-  chatTo: "GS Trọng",
-  activeSender: '#gs_chat'
 }];
 
 const chatModule = ChatModule.getInstance({
   chatMessagesDefault,
   chatMessages,
   notifications,
-  activeSender: '#gs_chat'
+  activeSender: '#tuan_chat'
 });
 // Wait for the document to be fully loaded before adding the event listener
 document.addEventListener('DOMContentLoaded', chatModule.onLoad);
